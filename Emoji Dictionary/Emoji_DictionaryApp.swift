bd4e510ba03ccf81efe5228259e3daab66aacfbb
Sean Bain <75031999+SEANYB4@users.noreply.github.com>
2021-12-02 15:25:11 +0000

    Initial Commit

---
+++ Emoji Dictionary/Emoji_DictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_DictionaryApp.swift
+//  Emoji Dictionary
+//
+//  Created by Sean Bain on 02/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_DictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
